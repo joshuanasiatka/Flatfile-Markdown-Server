af1ae29eded2a262e045f5227e2718552a06965d
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-10-27 23:32:28 -0400

    Fix for HTTPS
    Had to download strapdown.js from source since that site doesn’t
    support HTTPS for embedding.

--- app/app.php
+++ app/app.php
@@ -11,7 +11,7 @@ render_markdown();
 ?>
 
 <!-- Strapdown (http://strapdownjs.com/) -->
-<script src="http://strapdownjs.com/v/0.2/strapdown.js"></script>
+<script src="../../src/js/strapdown.js"></script>
 <!-- Strapdown Topbar (https://github.com/joedf/strapdown-topbar) -->
 <script src="../../src/js/strapdown-topbar.js"></script>
 </html>
